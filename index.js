96375bedfc66713a3e7d9a320fc4422b60b4b2d4
Bodo Minea <bodo@flashweb.ro>
2017-08-03 12:41:45 +0300

    Prepare cmd usage

--- index.js
+++ index.js
@@ -9,26 +9,37 @@ const express = require('express'),
       fs = require('fs'),
       resemble = require('node-resemble-js');
 
+var folder,mode;
+
+process.argv.forEach(function (val, index, array) {
+  if(index==2){ folder = val; }
+  if(index==3){ mode = val; }
+});
+
 function datelog(){
 	return Date().split(' ')[0]+' '+Date().split(' ')[1]+' '+Date().split(' ')[2]+' '+Date().split(' ')[3]+' '+Date().split(' ')[4];
 }
 
 bonjour.publish({ name: 'GALACIUCRECOGNIZER', type: 'http', port: 42522 }) // announce
 
-app.use('/static', express.static('res/public')); // resurse statice
+if(!folder){
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/app/');
-  console.log('Webapp accessed -> '+datelog());
-});
+	app.use('/static', express.static('res/public')); // resurse statice
 
-app.get('/picmatch', function (req, res) { // API-ul, creierul, aka Shakespeare
-   console.log('Almighty API called -> '+datelog());
-   res.send('');
-})
+	app.get('/', function(req, res){ // interfata online
+	  res.sendFile(__dirname + '/app/');
+	  console.log('Webapp accessed -> '+datelog());
+	});
 
-app.listen(42522, function () {
-  require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-  		console.log('API Listening on - '+add+':42522 -> '+datelog());
+	app.get('/picmatch', function (req, res) { // API-ul, creierul, aka Shakespeare
+	   console.log('Almighty API called -> '+datelog());
+	   res.send('');
 	})
-})
+
+	app.listen(42522, function () {
+	  require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+	  		console.log('API Listening on - '+add+':42522 -> '+datelog());
+		})
+	})
+
+}
