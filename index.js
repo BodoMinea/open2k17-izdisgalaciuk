b5621ef9ef3adb57d5b0873a698d65078e3db555
Bodo Minea <bodo@flashweb.ro>
2017-08-03 19:08:04 +0300

    Time measurement and logging

--- index.js
+++ index.js
@@ -42,7 +42,8 @@ function arrayavg(elmt){
 	return avg;
 }
 
-function dodiff(input,iname,start,stop,recall){
+function dodiff(input,iname,start,stop,recall,setime){
+  if(!setime){ var stime = Date.now(); } else { stime=setime; }
   if(!start){ var start=0; stop=3; }
   var results=[], iterations=0,running=1;
   fs.readdir('./res/'+fselect+'set', (err, files) => {
@@ -51,10 +52,10 @@ function dodiff(input,iname,start,stop,recall){
     	iterations++;
 		results.push(100-data.misMatchPercentage);
 		console.log(results);
-		console.log('WORKING ('+iname+') -> Nr. Rez.: '+results.length+' Avg: '+arrayavg(results)+' Max: '+results.max());
-		if(iterations==stop+1){ var avg = arrayavg(results); if(avg>50) { response((results.max()*3+avg)/4,iname); }
-		else { var rndx=randomize(iterations,files.length-2); dodiff(input,iname,rndx,rndx+2,true); } }
-		else if(recall&&results.length==2){ response((results.max()*3+avg)/4,iname); }
+		console.log('WORKING ('+iname+') -> Nr. Rez.: '+results.length+' Avg: '+arrayavg(results).toFixed(2)+' Max: '+results.max().toFixed(2));
+		if(iterations==stop+1){ var avg = arrayavg(results); if(avg>50) { response((results.max()*3+avg)/4,iname,((Date.now()-stime)/1000).toFixed(2)); }
+		else { var rndx=randomize(iterations,files.length-2); dodiff(input,iname,rndx,rndx+2,true,setime); } }
+		else if(recall&&results.length==2){ response((results.max()*3+avg)/4,iname,((Date.now()-stime)/1000).toFixed(2)); }
 		});
 	}
 })
@@ -64,26 +65,31 @@ function writeCSV () {
     var file = './raport.csv';
     var text = '';
     for(var i=0;i<finalresp.length;i++) { 
+    	if(i!=finalresp.length-1) { text+=finalresp[i].split('     ')[1]+','; } else { text+=finalresp[i].split('     ')[1]; }
+    }
+    text+='\r\n';
+    for(var i=0;i<finalresp.length;i++) { 
     	if(i!=finalresp.length-1) { text+=finalresp[i].split('     ')[0]+','; } else { text+=finalresp[i].split('     ')[0]; }
     }
     text+='\r\n';
     for(var i=0;i<finalresp.length;i++) { 
-    	if(i!=finalresp.length-1) { text+=finalresp[i].split('     ')[1]+','; } else { text+=finalresp[i].split('     ')[1]; }
+    	if(i!=finalresp.length-1) { text+=finalresp[i].split('     ')[2]+','; } else { text+=finalresp[i].split('     ')[2]; }
     }
-    if(fs.writeFileSync(file, text)){ console.log('Rezultatele au fost salvate si in raport.csv'); }
+    fs.writeFileSync(file, text)
 }
 
-function response(resp,iname){
+function response(resp,iname,stime){
 	if(folder){ 
-		console.log('FINAL ('+iname+') :'+resp);
-		finalresp.push(iname+'     '+resp); 
+		console.log('FINAL ('+iname+'): '+resp.toFixed(2)+'% - '+stime+' sec');
+		finalresp.push(resp.toFixed(2)+'%     '+iname+'     '+stime+' sec'); 
 		if(finalresp.length==total){ 
 			console.log();
 			console.log('------------- [ RAPORT FINAL FOLDER ] -------------');
-			console.log('_____________ ( path fisier, procent) _____________');
-			for(var i=0;i<finalresp.length;i++) { console.log(folder+'/'+finalresp[i]); }
+			console.log('_____________ (procent,  path fisier) _____________');
+			for(var i=0;i<finalresp.length;i++) { console.log(finalresp[i].split('     ')[0]+'     '+folder+'/'+finalresp[i].split('     ')[1]); }
 			console.log();
 			writeCSV();
+			console.log('Rezultatele impreuna cu timpii de procesare au fost salvate si in raport.csv');
 			process.exit();
 		}
 	}
