f7ea11a9bfa432b5369a0d6f712c4a306ba1b28d
Bodo Minea <bodo@flashweb.ro>
2017-08-04 03:31:32 +0300

    Index comments

--- index.js
+++ index.js
@@ -12,31 +12,31 @@ const express = require('express'),
       fs = require('fs-extra'),
       path = require('path');
 
-var folder, mode='fast', fselect, finalresp=[], total;
+var folder, mode='fast', fselect, finalresp=[], total; // global
 
 function set(x){
 	total = x;
 }
 
-Array.prototype.max = function() {
+Array.prototype.max = function() { // extindem array-ul sa aiba max
   return Math.max.apply(null, this);
 };
 
-process.argv.forEach(function (val, index, array) {
+process.argv.forEach(function (val, index, array) { // argumente CLI
   if(index==2){ folder = val; }
   if(index==3&&typeof(val)!='undefined'){ mode = val; }
   if(mode=='fast') { fselect='light'; } else { fselect='init'; }
 });
 
-function datelog(){
+function datelog(){ // procesare format data pentru log
 	return Date().split(' ')[0]+' '+Date().split(' ')[1]+' '+Date().split(' ')[2]+' '+Date().split(' ')[3]+' '+Date().split(' ')[4];
 }
 
-function randomize(start,stop){
+function randomize(start,stop){ // un fel de randbetween
 	return Math.floor(Math.random() * stop) + start;
 }
 
-function arrayavg(elmt){
+function arrayavg(elmt){ // media unui array cu numere
 	var sum = 0;
 	for( var i = 0; i < elmt.length; i++ ){
 	    sum += parseInt( elmt[i], 10 );
@@ -45,66 +45,66 @@ function arrayavg(elmt){
 	return avg;
 }
 
-function dodiff(input,iname,start,stop,recall,setime){
-  if (typeof(input)=='string') { var namearr=input.split('.'); 
-  if(namearr[namearr.length-1]=='jpg'||namearr[namearr.length-1]=='jpeg'||namearr[namearr.length-1]=='JPG'||namearr[namearr.length-1]=='JPEG'){ 
-  	console.log('Procesare '+input+' ca imagine JPEG... (+conversie)');
+function dodiff(input,iname,start,stop,recall,setime){ // functia serioasa care face comparatie
+  if (typeof(input)=='string') { var namearr=input.split('.'); // extensie
+  if(namearr[namearr.length-1]=='jpg'||namearr[namearr.length-1]=='jpeg'||namearr[namearr.length-1]=='JPG'||namearr[namearr.length-1]=='JPEG'){ // format img
+  	console.log('Procesare '+input+' ca imagine JPEG... (+conversie)'); // logging 
   	var get = input;
-  	input = images(get).encode("png");
-  } else { console.log('Procesare '+input+' ca imagine PNG...'); } }
-  if(!start){ var start=0; stop=3; }
+  	input = images(get).encode("png"); // convertim si luam ca buffer
+  } else { console.log('Procesare '+input+' ca imagine PNG...'); } } // ceva logging
+  if(!start){ var start=0; stop=3; } // iterare default
   var results=[], iterations=0;
   fs.readdir('./res/'+fselect+'set', (err, files) => {
   	for(i=start;i<=stop;i++){
   	if(!setime){ var stime = Date.now(); } else { stime=setime; }
     var diff = resemble(input).compareTo('./res/'+fselect+'set/'+files[i]).ignoreAntialiasing().onComplete(function(data){
     	iterations++;
-		results.push(100-data.misMatchPercentage);
-		console.log(results);
-		console.log('WORKING ('+iname+') -> Nr. Rez.: '+results.length+' Avg: '+arrayavg(results).toFixed(2)+' Max: '+results.max().toFixed(2));
-		if(iterations==stop+1){ var avg = arrayavg(results); if(avg>50) { response((results.max()*3+avg)/4,iname,((Date.now()-stime)/1000).toFixed(2)); }
-		else { var rndx=randomize(iterations,files.length-2); dodiff(input,iname,rndx,rndx+2,true,setime); } }
-		else if(recall&&results.length==2){ response((results.max()*3+arrayavg(results))/4,iname,((Date.now()-stime)/1000).toFixed(2)); }
+		results.push(100-data.misMatchPercentage); // asemanarea ca procent
+		console.log(results); // logging again
+		console.log('WORKING ('+iname+') -> Nr. Rez.: '+results.length+' Avg: '+arrayavg(results).toFixed(2)+' Max: '+results.max().toFixed(2)); // and again
+		if(iterations==stop+1){ var avg = arrayavg(results); if(avg>50) { response((results.max()*3+avg)/4,iname,((Date.now()-stime)/1000).toFixed(2)); } // returnam
+		else { var rndx=randomize(iterations,files.length-2); dodiff(input,iname,rndx,rndx+2,true,setime); } } // continuam pe un subset random de sample-uri
+		else if(recall&&results.length==2){ response((results.max()*3+arrayavg(results))/4,iname,((Date.now()-stime)/1000).toFixed(2)); } // returnam
 		});
 	}
 })
 }
 
-function writeCSV () {							
+function writeCSV () { // raport csv					
     var file = './raport.csv';
     var text = '';
-    for(var i=0;i<finalresp.length;i++) { 
+    for(var i=0;i<finalresp.length;i++) { // append nume fisiere
     	if(i!=finalresp.length-1) { text+=finalresp[i].split('     ')[1]+','; } else { text+=finalresp[i].split('     ')[1]; }
     }
     text+='\r\n';
-    for(var i=0;i<finalresp.length;i++) { 
+    for(var i=0;i<finalresp.length;i++) { // append procentaje
     	if(i!=finalresp.length-1) { text+=finalresp[i].split('     ')[0]+','; } else { text+=finalresp[i].split('     ')[0]; }
     }
     text+='\r\n';
-    for(var i=0;i<finalresp.length;i++) { 
+    for(var i=0;i<finalresp.length;i++) { // append 
     	if(i!=finalresp.length-1) { text+=finalresp[i].split('     ')[2]+','; } else { text+=finalresp[i].split('     ')[2]; }
     }
-    fs.writeFileSync(file, text)
+    fs.writeFileSync(file, text) // scriem si asteptam operatiunea
 }
 
-function response(resp,iname,stime){
-	if(folder){ 
+function response(resp,iname,stime){ // raspuns
+	if(folder){ // cli
 		console.log('FINAL ('+iname+'): '+resp.toFixed(2)+'% - '+stime+' sec');
 		finalresp.push(resp.toFixed(2)+'%     '+iname+'     '+stime+' sec'); 
-		if(finalresp.length==total){ 
-			console.log();
-			console.log('------------- [ RAPORT FINAL FOLDER ] -------------');
+		if(finalresp.length==total){ // gata
+			console.log(); // break
+			console.log('------------- [ RAPORT FINAL FOLDER ] -------------'); // un kindof header
 			console.log('_____________ (procent,  path fisier) _____________');
 			for(var i=0;i<finalresp.length;i++) { console.log(finalresp[i].split('     ')[0]+'     '+folder+'/'+finalresp[i].split('     ')[1]); }
-			console.log();
-			writeCSV();
-			console.log('Rezultatele impreuna cu timpii de procesare au fost salvate si in raport.csv');
-			process.exit();
+			console.log(); // break
+			writeCSV(); // scriem CSV
+			console.log('Rezultatele impreuna cu timpii de procesare au fost salvate si in raport.csv'); // notice
+			process.exit(); // iesire graceful din script
 		}
-	}else{
-		var str = resp.toFixed(2)+'% potrivire cu Gălăciuc -> '+iname+' | Procesat în '+stime+' sec.';
-		io.emit('done', str);
-		console.log('Servit către client: '+str)
+	}else{ // web, emitem pe socket catre client html+js
+		var str = resp.toFixed(2)+'% potrivire cu Gălăciuc -> '+iname+' | Procesat în '+stime+' sec.'; // procesare string
+		io.emit('done', str); // emitem
+		console.log('Servit către client: '+str) // log
 	}
 }
 
@@ -121,10 +121,10 @@ if(!folder){ // mod app server API daca nu avem cmd line
 	  console.log('Webapp accesat -> '+datelog());
 	});
 
-	app.route('/upload')
+	app.route('/upload') // upload pentru Dropzone AJAx
     .post(function (req, res, next) {
 	var fstream;
-        req.pipe(req.busboy);
+        req.pipe(req.busboy); // pipe din middleware
         req.busboy.on('file', function (fieldname, file, filename) {
         	console.log('Minunatul API apelat -> '+datelog());
             console.log("Incarcare: " + filename);
@@ -132,21 +132,21 @@ if(!folder){ // mod app server API daca nu avem cmd line
             file.pipe(fstream);
             fstream.on('close', function () {    
                 console.log("Incarcare terminata " + filename + ' ' + datelog());              
-                dodiff('./res/upl/'+filename,filename);
+                dodiff('./res/upl/'+filename,filename); // lansam procesarea
             });
         });
     });
 
 	server.listen(42522, function () {
 	  require('dns').lookup(require('os').hostname(), function (err, add, fam) {
-	  		console.log('API Listening on - '+add+':42522 -> '+datelog());
+	  		console.log('API Listening on - '+add+':42522 -> '+datelog()); // log IP masina
 		})
 	})
 
 }else{
-	finalresp=[];
-	fs.readdir(folder, (err, files) => {
-  		for(var i=0; i<files.length; i++){
+	finalresp=[]; // init blank pentru array rezultate
+	fs.readdir(folder, (err, files) => { // iteram prin lista cu fisiere
+  		for(var i=0; i<files.length; i++){ 
   			dodiff(folder+'/'+files[i],files[i]);
   			set(files.length);
   		}
